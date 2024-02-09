49c2e8dfa6f0c9a2096e2779c408483e78b73280
Zoodasa <36983642+hyjun0407@users.noreply.github.com>
2024-02-09 19:08:49 +0900

    Update COMRaceConditionSeeker.py
    Get Internal/External Function Call Test

--- COMRaceConditionSeeker.py
+++ COMRaceConditionSeeker.py
@@ -1,15 +1,13 @@
 import idautils
 import idc
-import ida_segment
 
 current_function_start = idc.get_func_attr(idc.here(), idc.FUNCATTR_START)
 
 for instr in idautils.FuncItems(current_function_start):
     for ref in idautils.CodeRefsFrom(instr, False):
-        seg = ida_segment.getseg(ref)
-        if seg and seg.type == ida_segment.SEG_DATA:
-            func_addr = idc.get_qword(ref)
-            if func_addr:
-                func_name = idc.get_func_name(func_addr)
-                if func_name:
-                    print("간접 호출된 함수: {}".format(func_name))
+        seg_name = idc.get_segm_name(ref)
+        func_name = ida_name.get_name(ref)
+        if func_name and seg_name == '.idata':
+            print("Internal Func call(내부): {}".format(func_name))
+        elif func_name and idc.get_func_attr(ref, idc.FUNCATTR_START) != current_function_start:
+            print("Out(Ex) Func call(외부): {}".format(func_name))
