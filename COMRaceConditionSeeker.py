2dcea8d9eba73f2bbf028c194f51e2c24fd98acc
Zoodasa <36983642+hyjun0407@users.noreply.github.com>
2024-02-10 14:58:14 +0900

    change indent issue

--- COMRaceConditionSeeker.py
+++ COMRaceConditionSeeker.py
@@ -20,7 +20,7 @@ for func in idautils.Functions():
             # 참조된 주소가 현재 분석 중인 함수의 범위 외부에 있고, `.idata` 세그먼트에 있으면 외부 함수로 간주합니다.
             if ref_func_name and seg_name == '.idata':
                 if ref_func_name == '__imp_ReleaseSRWLockExclusive':
-                print("{} 함수에서 외부 함수 참조: {}".format(current_function_name, ref_func_name))
+                    print("{} 함수에서 외부 함수 참조: {}".format(current_function_name, ref_func_name))
             # 참조된 주소가 현재 분석 중인 함수의 범위 외부에 있지만, `.idata` 세그먼트에는 없는 경우, 내부 함수로 간주합니다.
             elif ref_func_name and idc.get_func_attr(ref, idc.FUNCATTR_START) != current_function_start:
                 print("{} 함수에서 내부 함수 참조: {}".format(current_function_name, ref_func_name))
